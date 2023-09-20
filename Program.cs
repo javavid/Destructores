0467aa3bb657912c46349314d43d1a6cafbb4b47
David jimenez <ederjimeneztapias@gmail.com>
2023-09-20 16:24:57 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Destructores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ManejoArchivos manejoArchivos1 = new ManejoArchivos();
+            manejoArchivos1.Mensaje();
+        }
+    }
+
+    class ManejoArchivos
+    {
+        StreamReader archivo = null;
+
+        int contador = 0;
+        string linea;
+
+        public ManejoArchivos()
+
+        {
+            archivo = new StreamReader(@"C:\Users\saram\OneDrive\Escritorio\eder tapias.txt");
+
+            while ((linea = archivo.ReadLine()) != null) 
+            {
+             contador++;
+            }
+        }
+
+        public void Mensaje()
+        {
+            Console.WriteLine("hay {0} lineas", contador);
+        }
+
+        ~ManejoArchivos()
+        {
+            archivo.Close();
+        }
+    }
+}
